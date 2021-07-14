11c7cc1da82d9dd3ed7d8b225296b90262f3a0e6
Mecaneer23 <74385377+Mecaneer23@users.noreply.github.com>
2021-07-13 22:44:43 -0500

    remove unnecessary comments

--- BinarytoDecimal.cpp
+++ BinarytoDecimal.cpp
@@ -5,11 +5,8 @@ using namespace std;
 
 int BinaryToDecimal(long long binaryNumber) {
 	string value = to_string(binaryNumber);
-	// cout << value << " = value\n" << binaryNumber << " = binNum\n\n";
 	long output = 0;
 	for (long position = 0; position < value.length(); ++position) {
-		// cout << "value at position is: " << to_string(value.at(position)) << '\n' << value.at(position) << endl;
-		// cout << position << " = position" << endl;
 		if (value.at(position) == '1') {
 			if (position == value.length() - 1) {
 				output++;
@@ -24,17 +21,12 @@ int BinaryToDecimal(long long binaryNumber) {
 		}
 	}
 	return output;
-	// return 100;
 }
 
 int main() {
 	string input;
 	cout << "Binary Number: ";
 	getline(cin, input);
-	// cout << input;
-	// string temp = input.substr(13);
-	// int num = stoi(temp);
-	// cout << stoi(input) << " = stoi input" << endl;
 	long long int num = stoll(input);
 	cout << BinaryToDecimal(num) << endl;
 	return 0;
