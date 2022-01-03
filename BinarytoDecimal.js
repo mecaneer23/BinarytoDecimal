bb8da4a9996769d13881ff94a67ec263c7bf16ec
Mecaneer23 <74385377+Mecaneer23@users.noreply.github.com>
2022-01-02 21:08:52 -0600

    update and refactor js

--- BinarytoDecimal.js
+++ BinarytoDecimal.js
@@ -1,11 +1,13 @@
 function BinaryToDecimal(binaryNumber) {
 	var value = binaryNumber.toString(10);
+	var length = value.length;
 	var output = 0;
-	for (position = 0; position < value.length; position++) {
-		if (value.charAt(position) == '1') {
-			output += Math.pow(2, value.length - 1 - position);
-		} else if (value.charAt(position) == '0') {
-			
+	for (position = 0; position < length; position++) {
+		char = value.charAt(position);
+		if (char == '1') {
+			output += Math.pow(2, length - 1 - position);
+		} else if (char == '0') {
+			continue
 		} else {
 			return "Make sure you only input binary values";
 		}
