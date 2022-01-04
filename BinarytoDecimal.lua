3c26de8a9140df820e5e64b7bc26ed35132f7ab7
Mecaneer23 <74385377+Mecaneer23@users.noreply.github.com>
2022-01-03 22:05:11 -0600

    refactor lua

--- BinarytoDecimal.lua
+++ BinarytoDecimal.lua
@@ -1,17 +1,16 @@
---[[
-cd C:\Users\user\Google Drive\Code\LanguageTranslators\BinaryDecimal\DONE && "F:\Program Files\Binaries-LuaDist-batteries-0.9.8-Windows-x86\bin\lua.exe" BinarytoDecimal.lua
--- ]]
+#!/usr/bin/env lua
 function BinarytoDecimal(binaryNumber)
 	value = tostring(binaryNumber)
+	length = string.len(value)
 	output = 0
-	for position = 0, string.len(value) do
+	for position = 0, length do
 		if string.sub(value, position, position) == "1" then
-			output = output + 2^(string.len(value) - 1 - position)
+			output = output + 2^(length - position)
 		elseif string.sub(value, position, position) == "0" then
 
 		end
 	end
-	return output*2
+	return math.floor(output)
 end
 
 io.write("Binary Number: ")
