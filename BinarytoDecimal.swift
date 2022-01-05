e40838cd89e8ead585d0513fcb3cea6f25f666a8
Mecaneer23 <74385377+Mecaneer23@users.noreply.github.com>
2022-01-04 21:51:16 -0600

    refactor swift

--- BinarytoDecimal.swift
+++ BinarytoDecimal.swift
@@ -2,15 +2,17 @@ import Foundation;
 
 func BinarytoDecimal(_ binaryNumber: Int) -> Int {
 	let value:String = String(binaryNumber);
+	let len:Int = value.count;
 	var output:Int = 0;
-	for position in 0...value.count-1 {
-		if (value[value.index(value.startIndex, offsetBy: position)] == "1") {
+	for position in 0...len-1 {
+		let char:Character = value[value.index(value.startIndex, offsetBy: position)];
+		if (char == "1") {
 			let decimal_two = Double(2);
-			let power_of = Double(value.count-1-position)
+			let power_of = Double(len-1-position)
 			let temp = pow(decimal_two, power_of);
 			let result = NSDecimalNumber(value: temp);
 			output += Int(result);
-		} else if (value[value.index(value.startIndex, offsetBy: position)] == "0") {
+		} else if (char == "0") {
 			
 		} else {
 			print("Make sure you only input binary values");
