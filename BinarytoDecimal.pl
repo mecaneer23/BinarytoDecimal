0766a385f6dbdfa5a5e79a0f8b1351603fb63764
Mecaneer23 <74385377+Mecaneer23@users.noreply.github.com>
2022-01-04 21:39:18 -0600

    refactor perl, may need to change line endings again

--- BinarytoDecimal.pl
+++ BinarytoDecimal.pl
@@ -3,12 +3,14 @@ use warnings;
 
 sub BinarytoDecimal() {
   my $value = "@_";
+  my $len = length($value);
   my $output = 0;
-  for (my $position = 0; $position < length($value); $position++) {
-    if (substr($value, $position, 1) == "1") {
-      $output += 2**(length($value) - 1 - $position);
-    } elsif (substr($value, $position, 1) == "0") {
-
+  for (my $position = 0; $position < $len; $position++) {
+    my $char = substr($value, $position, 1);
+    if ($char == "1") {
+      $output += 2**($len - 1 - $position);
+    } elsif ($char == "0") {
+    
     } else {
       return "Make sure you only input binary values";
     }
