1335cee0032fdec7273759855be1b3b47d551ade
Mecaneer23 <74385377+Mecaneer23@users.noreply.github.com>
2022-01-04 21:36:58 -0600

    refactor php

--- BinarytoDecimal.php
+++ BinarytoDecimal.php
@@ -1,11 +1,13 @@
 <?php
 function BinarytoDecimal($binaryNumber) {
 	$value = strval($binaryNumber);
+	$length = strlen($value);
 	$output = 0;
-	for ($position = 0; $position < strlen($value); $position++) {
-		if (substr($value, $position, 1) == '1') {
-			$output = intval($output) + intval(2**(strlen($value) - 1 - $position));
-		} else if (substr($value, $position, 1) == '0') {
+	for ($position = 0; $position < $length; $position++) {
+		$char = substr($value, $position, 1);
+		if ($char == '1') {
+			$output = intval($output) + intval(2**($length - 1 - $position));
+		} else if ($char == '0') {
 			
 		} else {
 			return "Make sure you only input binary values";
