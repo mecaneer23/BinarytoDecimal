af56cefdbb46389ed5591bb32b9121d144fcf68b
crab <92041779+crabbo-rave@users.noreply.github.com>
2022-01-07 16:10:43 -0500

    Update BinarytoDecimal.fsx
    Made it more idiomatic

--- BinarytoDecimal.fsx
+++ BinarytoDecimal.fsx
@@ -1,18 +1,14 @@
 // dotnet fsi BinarytoDecimal.fsx
 module BinarytoDecimal =
 
-    let BinaryDecimal(binaryNumber) =
-        let value = binaryNumber.ToString()
-        let length = value.Length
-        let mutable output = 0
-        let mutable char = '0'
-        for position in 0..length-1 do
-            char <- value.Chars(position)
-            match char with
-            | '1' -> output <- output + (int (2.0**(float (length - position - 1))))
-            | '0' -> output <- output
-            | _ -> failwith "Make sure you only input binary values"
-        output
+    let binaryToDecimal binaryNumber =
+      let rec recurse (left: list<char>) accumulator =
+        match left with
+        | [] -> accumulator
+        | '1'::xs -> recurse xs (accumulator + (int (2. ** (float (left.Length - 1)))))
+        | '0'::xs -> recurse xs accumulator
+        | _ -> failwith "Make sure you only give '1' or '0' values"
+      recurse (Seq.toList binaryNumber) 0
 
     printf "Binary Number: "
     let input = System.Console.ReadLine()
