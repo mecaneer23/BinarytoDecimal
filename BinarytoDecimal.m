57e912d013479de745e9948c6d4c8ae6d61db8bf
Mecaneer23 <Mecaneer23@gmail.com>
2022-03-28 10:00:37 -0500

    add objective c

---
+++ BinarytoDecimal.m
@@ -0,0 +1,38 @@
+/* gcc BinarytoDecimal.m -objc -std=c99 -lm */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int BinarytoDecimal(int binaryNumber) {
+    if (binaryNumber == 0) {
+        return 0;
+    }
+    int len = floor(log10(binaryNumber)) + 1;
+    char value[len + 1];
+    sprintf(value, "%d", binaryNumber);
+    int output = 0;
+    for (int position = 0; position < len; position++) {
+        char chr = value[position];
+        if (chr == '1') {
+            output += pow(2, len - 1 - position);
+        } else if (chr == '0') {
+
+        } else {
+            return -1;
+        }
+    }
+    return output;
+}
+
+int main(void) {
+    int num;
+    printf("%s", "Binary Number: ");
+    scanf("%d", &num);
+    int result = BinarytoDecimal(num);
+    if (result == -1) {
+        printf("%s\n", "Make sure you only input binary values");
+    } else {
+        printf("%d\n", result);
+    }
+}
