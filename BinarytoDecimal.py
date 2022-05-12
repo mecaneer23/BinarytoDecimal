da7ae8f2c2c5c3de92446eb8569b421dc4b732e1
mecaneer23 <mecaneer23@gmail.com>
2022-05-11 20:42:59 -0500

    tabs to spaces

--- BinarytoDecimal.py
+++ BinarytoDecimal.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python3
 def BinarytoDecimal(binaryNumber):
-	value = str(binaryNumber)
-	length = len(value)
-	output = 0
-	for position in range(length):
-		char = value[position]
-		if char == '1':
-			output += 2**(length - 1 - position)
-		elif char == '0':
-			continue
-		else:
-			return "Make sure you only input binary values"
-	return output
+    value = str(binaryNumber)
+    length = len(value)
+    output = 0
+    for position in range(length):
+        char = value[position]
+        if char == '1':
+            output += 2**(length - 1 - position)
+        elif char == '0':
+            continue
+        else:
+            return "Make sure you only input binary values"
+    return output
 
 if __name__ == "__main__":
-	print(BinarytoDecimal(int(input("Binary Number: "))))
+    print(BinarytoDecimal(int(input("Binary Number: "))))
