990fb5758d42639eaf1b630a9a83010e261796af
Mecaneer23 <Mecaneer23@gmail.com>
2022-06-10 17:54:30 -0500

    make more readable

--- BinarytoDecimal.hs
+++ BinarytoDecimal.hs
@@ -6,8 +6,15 @@ if_check ('1', len, pos) = (2 ^ (len - pos - 1))
 if_check (char, len, pos) = error "Make sure you only input binary values"
 
 binary_to_decimal :: Int -> Int
-binary_to_decimal binary_number = 
-    sum [if_check ((show binary_number) !! position, length (show binary_number), position) | position <- [0..length (show binary_number) - 1]]
+binary_to_decimal binary_number =
+    sum [
+        if_check (
+            (show binary_number) !! position,
+            length (show binary_number),
+            position
+        ) |
+        position <- [0..length (show binary_number) - 1]
+    ]
 
 main :: IO ()
 main = do
