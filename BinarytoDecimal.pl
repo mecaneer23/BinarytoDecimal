46f06191d92b72ba29348cf6666356220789fd47
mecaneer23 <mecaneer23@gmail.com>
2022-07-18 12:21:29 -0500

    make more readable

--- BinarytoDecimal.pl
+++ BinarytoDecimal.pl
@@ -1,21 +1,19 @@
 #!/bin/perl
-use warnings;
-
 sub BinarytoDecimal() {
-  my $value = "@_";
-  my $len = length($value);
-  my $output = 0;
-  for (my $position = 0; $position < $len; $position++) {
-    my $char = substr($value, $position, 1);
-    if ($char == "1") {
-      $output += 2**($len - 1 - $position);
-    } elsif ($char == "0") {
-    
-    } else {
-      return "Make sure you only input binary values";
+    my $value = "@_";
+    my $len = length($value);
+    my $output = 0;
+    for (my $position = 0; $position < $len; $position++) {
+        my $char = substr($value, $position, 1);
+        if ($char == "1") {
+            $output += 2**($len - 1 - $position);
+        } elsif ($char == "0") {
+            $output += 0;
+        } else {
+            return "Make sure you only input binary values";
+        }
     }
-  }
-  return $output;
+    return $output;
 }
 
 print("Binary Number: ");
