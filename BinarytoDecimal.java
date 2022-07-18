378bb608c8ed1da9ddf690b10eea11c0940b7143
mecaneer23 <mecaneer23@gmail.com>
2022-07-18 12:32:41 -0500

    make more readable

--- BinarytoDecimal.java
+++ BinarytoDecimal.java
@@ -1,31 +1,27 @@
-package BinarytoDecimal;
-
 import java.util.Scanner;
 
 public class BinarytoDecimal {
-	public static void main(String[] args) {
-		System.out.print("Binary Number: ");
-		Scanner inp = new Scanner(System.in);
-		String input = inp.nextLine();
-		inp.close();
-		Long num = Long.parseLong(input);
-		System.out.println(BinaryToDecimal(num));
-	}
-	public static int BinaryToDecimal(Long binaryNumber) {
-		String value = Long.toString(binaryNumber);
-		int length = value.length();
-		int output = 0;
-		for (int position = 0; position < length; position++) {
-			char chr = value.charAt(position);
-			if (chr == '1') {
-				output += Math.pow(2, length - 1 - position);
-			} else if (chr == '0') {
-
-			} else {
-				System.err.println("Make sure you only input binary values");
-				System.exit(1);
-			}
-		}
-		return output;
-	}
+    public static int BinarytoDecimal(Long binaryNumber) {
+        String value = Long.toString(binaryNumber);
+        int length = value.length();
+        int output = 0;
+        for (int position = 0; position < length; position++) {
+            char chr = value.charAt(position);
+            if (chr == '1') {
+                output += Math.pow(2, length - 1 - position);
+            } else if (chr == '0') {
+                continue;
+            } else {
+                System.err.println("Make sure you only input binary values");
+                System.exit(1);
+            }
+        }
+        return output;
+    }
+    public static void main(String[] args) {
+        System.out.print("Binary Number: ");
+        Scanner inp = new Scanner(System.in);
+        System.out.println(BinarytoDecimal(inp.nextLong()));
+        inp.close();
+    }
 }
