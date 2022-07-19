30b8390ceed1f8c943438dc9da46749eb115f5e3
mecaneer23 <mecaneer23@gmail.com>
2022-07-18 22:36:27 -0500

    make more readable and slightly refactor

--- BinarytoDecimal.go
+++ BinarytoDecimal.go
@@ -8,6 +8,7 @@ package main
 import (
 	"fmt"
 	"math"
+	"os"
 )
 
 func BinarytoDecimal(binaryNumber string) int {
@@ -19,22 +20,18 @@ func BinarytoDecimal(binaryNumber string) int {
 		if char == "1" {
 			output += int(math.Pow(float64(2), float64(length-(1+position))))
 		} else if char == "0" {
-
+			continue
 		} else {
-			return -1
+			fmt.Println("Make sure you only input binary values")
+			os.Exit(0)
 		}
 	}
 	return output
 }
 
 func main() {
-	fmt.Print("Binary Number: ")
 	var input string
+	fmt.Print("Binary Number: ")
 	fmt.Scanln(&input)
-	result := BinarytoDecimal(input)
-	if result == -1 {
-		fmt.Println("Make sure you only input binary values")
-	} else {
-		fmt.Println(result)
-	}
+	fmt.Println(BinarytoDecimal(input))
 }
