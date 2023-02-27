4b055ee0e56edd112071f989b231224dcad87615
mecaneer23 <mecaneer23@gmail.com>
2023-02-27 08:31:55 -0600

    if_check function works

--- not_done/BinarytoDecimal.erl
+++ not_done/BinarytoDecimal.erl
@@ -2,12 +2,17 @@
 % executable file has .beam extension
 
 -module('BinarytoDecimal').
--export([start/0]).
+-import(math,[pow/2]).
+-export([if_check/3, binarytoDecimal/1, start/0]).
 
 start() ->
    io:fwrite("Hello, world!\n").
 
-% if_check(c)
+if_check("0", len, pos) -> 0.
+if_check("1", len, pos) -> pow(2, len - pos - 1);
+if_check(char, len, pos) -> "Make sure you only input binary values";
 
-% 'BinarytoDecimal'(binaryNumber) ->
-%     sum(if_check)
+% while()
+
+binarytoDecimal(binaryNumber) ->
+    value = integer_to_list(binaryNumber).
