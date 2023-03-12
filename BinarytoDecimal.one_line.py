54a13d1529e2a9309868ba7d1efdcf9061d2fdc9
mecaneer23 <mecaneer23@gmail.com>
2023-03-12 17:29:07 -0500

    code golf-ify one line

--- BinarytoDecimal.one_line.py
+++ BinarytoDecimal.one_line.py
@@ -1,20 +1,3 @@
 #!/usr/bin/env python3
 
-import sys
-
-print(
-    (
-        lambda binary_number: sum(
-            [
-                (
-                    lambda char, length, position: (
-                        2 ** (length - 1 - position) * int(char)
-                    )
-                    if char in "01"
-                    else sys.exit("Make sure you only input binary values")
-                )(str(binary_number)[position], len(str(binary_number)), position)
-                for position in range(len(str(binary_number)))
-            ]
-        )
-    )(int(input("Binary Number: ")))
-)
+print((lambda n:sum([(lambda c,l,i:(2**(l-1-i)*int(c))if c in"01"else exit("Make sure you only input binary values"))(str(n)[i],len(str(n)),i)for i in range(len(str(n)))]))(int(input("Binary Number: "))))
