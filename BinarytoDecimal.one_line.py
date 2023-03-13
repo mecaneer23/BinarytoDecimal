5208fcf8e60be5b5cc6951127e3973610eb3b34b
mecaneer23 <mecaneer23@gmail.com>
2023-03-12 20:10:26 -0500

    make slightly more readable

--- BinarytoDecimal.one_line.py
+++ BinarytoDecimal.one_line.py
@@ -1,3 +1,3 @@
 #!/usr/bin/env python3
 
-print((lambda n:sum([(lambda c,l,i:(2**(l-1-i)*int(c))if c in"01"else exit("Make sure you only input binary values"))(str(n)[i],len(str(n)),i)for i in range(len(str(n)))]))(int(input("Binary Number: "))))
+print((lambda x:(lambda n:sum([(lambda c,l,i:(2**(l-1-i)*int(c))if c in"01"else exit("Make sure you only input binary values"))(n[i],len(n),i)for i in range(len(n))]))(str(x)))(int(input("Binary Number: "))))
